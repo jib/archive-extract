325355bbc662b5a84866b0e2d0fe6500e8534d2b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-01-26 21:36:29 +0000

    Update CHANGES and bump version

--- lib/Archive/Extract.pm
+++ lib/Archive/Extract.pm
@@ -48,7 +48,7 @@ use vars qw[$VERSION $PREFER_BIN $PROGRAMS $WARN $DEBUG
             $_ALLOW_BIN $_ALLOW_PURE_PERL $_ALLOW_TAR_ITER
          ];
 
-$VERSION            = '0.62';
+$VERSION            = '0.64';
 $PREFER_BIN         = 0;
 $WARN               = 1;
 $DEBUG              = 0;
