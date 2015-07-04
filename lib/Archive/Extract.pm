8c8359868b1e152337213e33b4c29157b7951041
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2015-07-04 18:37:30 +0100

    Resolve RT#105425 Setting $/ to a REGEXP reference is forbidden

--- lib/Archive/Extract.pm
+++ lib/Archive/Extract.pm
@@ -1111,8 +1111,8 @@ sub _unzip_bin {
             ### Annoyingly, pesky MSWin32 can either have 'native' tools
             ### which have \r\n line endings or Cygwin-based tools which
             ### have \n line endings. Jan Dubois suggested using this fix
-            local $/ = ON_WIN32 ? qr/\r?\n/ : "\n";
-            $self->files( [split $/, $buffer] );
+            my $split = ON_WIN32 ? qr/\r?\n/ : "\n";
+            $self->files( [split $split, $buffer] );
         }
     }
 
