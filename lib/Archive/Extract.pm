4d42e52d232bcceaac4ede1edcfa71bd183d7060
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2021-05-06 14:12:53 +0100

    Release engineering for 0.88

--- lib/Archive/Extract.pm
+++ lib/Archive/Extract.pm
@@ -48,7 +48,7 @@ use vars qw[$VERSION $PREFER_BIN $PROGRAMS $WARN $DEBUG
             $_ALLOW_BIN $_ALLOW_PURE_PERL $_ALLOW_TAR_ITER
          ];
 
-$VERSION            = '0.86';
+$VERSION            = '0.88';
 $PREFER_BIN         = 0;
 $WARN               = 1;
 $DEBUG              = 0;
